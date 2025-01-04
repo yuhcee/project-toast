4cf3bc0bbc4eb5be271a3424c69dc1dbf46502d8
yuhcee <egbouchenna001@gmail.com>
2025-01-04 23:22:35 +0100

    Refactor Toast component to accept variant and message as props for better flexibility

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -1,38 +1,30 @@
 import React from 'react';
-import {
-  AlertOctagon,
-  AlertTriangle,
-  CheckCircle,
-  Info,
-  X,
-} from 'react-feather';
-
+import { AlertOctagon, AlertTriangle, CheckCircle, Info, X } from 'react-feather';
 import VisuallyHidden from '../VisuallyHidden';
-
 import styles from './Toast.module.css';
 
 const ICONS_BY_VARIANT = {
-  notice: Info,
-  warning: AlertTriangle,
-  success: CheckCircle,
-  error: AlertOctagon,
+    notice: Info,
+    warning: AlertTriangle,
+    success: CheckCircle,
+    error: AlertOctagon,
 };
 
-function Toast() {
-  return (
-    <div className={`${styles.toast} ${styles.notice}`}>
-      <div className={styles.iconContainer}>
-        <Info size={24} />
-      </div>
-      <p className={styles.content}>
-        16 photos have been uploaded
-      </p>
-      <button className={styles.closeButton}>
-        <X size={24} />
-        <VisuallyHidden>Dismiss message</VisuallyHidden>
-      </button>
-    </div>
-  );
+function Toast({ variant, message, onClose }) {
+    const Icon = ICONS_BY_VARIANT[variant];
+
+    return (
+        <div className={`${styles.toast} ${styles[variant]}`}>
+            <div className={styles.iconContainer}>
+                <Icon size={24} />
+            </div>
+            <p className={styles.content}>{message}</p>
+            <button className={styles.closeButton}>
+                <X size={24} onClick={onClose} />
+                <VisuallyHidden>Dismiss message</VisuallyHidden>
+            </button>
+        </div>
+    );
 }
 
 export default Toast;
